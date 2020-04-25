48a0678bcbfa4cb1b3f26244e2e302985b8f8f33
Hung Tran <hung@0x0.ninja>
2020-04-25 14:49:37 -0500

    add user details

--- lib/til_web/live/post_live/post_component.ex
+++ lib/til_web/live/post_live/post_component.ex
@@ -8,6 +8,18 @@ defmodule TilWeb.PostLive.PostComponent do
         <div class="rev-Row">
           <div class="rev-Col">
             <h2><%= live_redirect @post.title, to: Routes.post_show_path(@socket, :show, @post) %></h2>
+            <div class="MediaObject">
+              <div class="MediaObject-section">
+                <div class="Image-placeholder">
+                  <h3>h</h3>
+                </div>
+              </div>
+
+              <div class="MediaObject-section">
+                <%= @post.user_id %>
+                <%= @post.inserted_at %>
+              </div>
+            </div>
           </div>
         </div>
       </div>
