99a167e9d7a7325292a6833f39c5439904aec1cb
Hung Tran <hung@0x0.ninja>
2020-04-28 17:00:45 -0500

    remove requirement for password confirmation on registration

--- lib/til/users/user.ex
+++ lib/til/users/user.ex
@@ -1,11 +1,13 @@
 defmodule Til.User do
   use Ecto.Schema
   use Pow.Ecto.Schema, password_hash_methods: {&Bcrypt.hash_pwd_salt/1, &Bcrypt.verify_pass/2}
+  import Pow.Ecto.Schema.Changeset, only: [new_password_changeset: 3]
 
   use Pow.Extension.Ecto.Schema,
     extensions: [PowResetPassword, PowEmailConfirmation]
 
   use Adminable
+
   import Ecto.{Query, Changeset}, warn: false
 
   @type t :: %__MODULE__{}
@@ -22,7 +24,9 @@ defmodule Til.User do
 
   def changeset(schema, attrs \\ %{}) do
     schema
-    |> pow_changeset(attrs)
+    |> pow_user_id_field_changeset(attrs)
+    |> pow_current_password_changeset(attrs)
+    |> new_password_changeset(attrs, @pow_config)
     |> pow_extension_changeset(attrs)
     |> cast(attrs, [:name])
   end
